628ac35e85c4fe527ca6ac3fb401b25a653f164d
rogeliocoding-web <rogeliocoding@gmail.com>
2026-05-08 16:00:31 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
+import React from "react";
 import "./App.css";
 import { auth } from "./firebase/init";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, []);
+
   function register() {
     console.log("register");
-    createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
-    .then ((user) => {
-      console.log (user)
-    })
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then((user) => {
+        console.log(user);
+      })
 
-     .catch((error) => {
-    console.log(error)
-  })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function login() {
+    signInWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then(({ user }) => {})
+
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+
   return (
     <div className="App">
-      <button onClick={register}>Register</button>
-      <button>Log in</button>
+      <div className="nav__bar">
+        <button onClick={register}>Register</button>
+        <button onClick={login}>Login</button>
+        {loading ? "Loading..." : user.email}
+
+        <button className="first__letter" onClick={logout}>
+          {user.email ? user.email[0].toUpperCase() : ""}
+        </button>
+      </div>
     </div>
   );
-}
 }
 
 export default App;
